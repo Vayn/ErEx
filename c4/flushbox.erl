19ef6ddd9164093656e3a180e0c6dff954b7f46b
Vayn a.k.a. VT <vayn@vayn.de>
2012-03-11 23:04:46 +0800

    Flush mailbox

---
+++ c4/flushbox.erl
@@ -0,0 +1,20 @@
+-module(flushbox).
+-export([read/1, flush/1]).
+
+%% Demonstrate how to flush mailbox
+
+read({Db, Key}) ->
+  flush(),
+  Db ! {self(), {read, Key}},
+  receive
+    {read, R} -> {ok, R};
+    {error, Reason} => {error, Reason}
+  after 1000 -> {error, timeout}
+  end.
+
+flush() ->
+  receive
+    {read, _} -> flush();
+    {error, _} -> flush()
+  after 0 -> ok
+  end.
